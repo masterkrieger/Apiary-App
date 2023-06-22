9dd2094e97089952c9cd3014e47944d784acdf13
Jeremy Barr <jeremybarr23@gmail.com>
2023-06-22 12:57:49 -0400

    Added CSS styles and Date format
    - Added style.css in Assets, imported to main.js
    - ApiaryChart.vue:
    -- Updated styles of components
    -- Update x-axis scale unit based on timeframe of data.
    - Updated App style background.

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: #FAD33D;
+  color: #BB3D04;
 }
 </style>
--- src/components/ApiaryChart.vue
+++ src/components/ApiaryChart.vue
@@ -1,47 +1,115 @@
- <template>
+<template>
   <div>
     <h1>Hive Weights</h1>
-    <div>
+    <div class="button-container">
       <button @click="setDaily">Day</button>
       <button @click="setWeekly">Week</button>
       <button @click="setMonthly">Month</button>
       <button @click="setYearly">Year</button>
     </div>
+    <div class="chart-container" style="position: relative; height:40vh;">
+      <Line ref="linechart" :key="chartKey" :data="chartData" :options="chartOptions" v-if="chartData" />
+    </div>
     <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
-    <div class="chart-container" style="position: relative; height:40vh; width:90vw">
-      <canvas id="apiaryChart"></canvas>
+    <div class="alert alert-info" v-if="loading">
+      Loading...
     </div>
   </div>
 </template>
 
 <script>
-import { Chart, LineController, LineElement, PointElement, LinearScale, TimeScale, Title, Legend, Tooltip } from 'chart.js';
-import axios from "axios";
+import { Chart as ChartJS, LineController, LineElement, PointElement, LinearScale, TimeScale, Title, Legend, Tooltip } from 'chart.js';
 import 'chartjs-adapter-date-fns';
+import { Line } from 'vue-chartjs';
+import axios from "axios";
 
-Chart.register(LineController, LineElement, PointElement, LinearScale, TimeScale, Title, Legend, Tooltip);
+ChartJS.register(LineController, LineElement, PointElement, LinearScale, TimeScale, Title, Legend, Tooltip );
 
 export default {
   name: 'apiaryChart',
+  components: { Line },
   data() {
     return {
-      chart: null,
-      ipAddress: '172.22.69.16:3001',
+      ipAddress: '172.20.83.124:3001',
       chartData: null,
       colorIndex: 0,
-      colorList: ['#FF6384', '#36A2EB', '#FFCE56', '#33FF9F', '#FF9F33', '#9F33FF'],
+      colorList: ['#FAD33D', '#DC893B', '#BB3D04', '#FFF472', '#DC893B', '#FAE7BF'],
+      chartKey: '', // Key for updating the chart component
       errorMessage: '',
+      loading: false,
     };
   },
   mounted() {
     this.setMonthly();
-    //this.renderChart();
+  },
+  computed: {
+    chartOptions() {
+      return {
+        responsive: true,
+        maintainAspectRatio: false,
+        tooltips: {
+          callbacks: {
+            label: (context) => {
+              let label = context.dataset.label || '';
+              if (label) {
+                label += ': ';
+              }
+              if (context.parsed.y !== null) {
+                label += context.parsed.y + ' lbs';
+              }
+              return label;
+            },
+          },
+        },
+        scales: {
+          x: {
+            type: 'time',
+            time: {
+              unit: 'day',
+              displayFormats: {
+                hour: 'HH, dd MMM',
+                day: 'dd MMM, yyyy',
+                month: 'MMM yyyy',
+              }
+            },
+            title: {
+              display: true,
+              text: 'Date'
+            }
+          },
+          y: {
+            beginAtZero: false,
+            title: {
+              display: true,
+              text: 'Weight (lbs)'
+            }
+          },
+        },
+      };
+    },
   },
   methods: {
     getCycledColor() {
       const color = this.colorList[this.colorIndex];
       this.colorIndex = (this.colorIndex + 1) % this.colorList.length;
       return color;
+    },
+    hexToRGBA(hex, alpha) {
+      // Remove the # symbol, if present
+      hex = hex.replace('#', '');
+
+      // Convert the hex value to RGB
+      const r = parseInt(hex.substring(0, 2), 16);
+      const g = parseInt(hex.substring(2, 4), 16);
+      const b = parseInt(hex.substring(4, 6), 16);
+
+      // Calculate the alpha value
+      alpha = alpha || 1;
+
+      // Create the RGBA color string
+      const rgba = `rgba(${r}, ${g}, ${b}, ${alpha})`;
+
+      return rgba;
     },
     getRandomColor() {
       const letters = '0123456789ABCDEF';
@@ -61,7 +129,6 @@ export default {
             this.errorMessage = 'No data available for the selected date.';
           } else {
             this.processHiveData(hiveData);
-            this.updateChart();
           }
         })
         .catch((error) => {
@@ -79,11 +146,15 @@ export default {
             x: new Date(item.date),
             y: item.weight,
           }));
+        
+        const colorCycle = this.getCycledColor();
 
         return {
           label: hive,
           data: data,
-          borderColor: this.getCycledColor(),
+          borderColor: colorCycle,
+          pointBackgroundColor: '#BB3D04',
+          backgroundColor: colorCycle,
           fill: false,
         };
       });
@@ -92,103 +163,95 @@ export default {
         datasets: datasets,
       };
     },
-    renderChart() {
-      //const ctx = this.$refs.apiaryChart.getContext('2d');
-      const ctx = document.getElementById('apiaryChart');
-
-      this.chart = new Chart(ctx, {
-        type: 'line',
-        data: this.chartData,
-        options: {
-          plugins: {
-            legend: {
-              labels: {
-                color: '#36495d'
-              }
-            },
-            tooltip: {
-              enabled: true,
-              callbacks: {
-                label: ((context) => {
-                  let label = context.dataset.label || '';
-                  if (label) {
-                    label += ': ';
-                  }
-                  if (context.parsed.y !== null) {
-                    label += context.parsed.y + ' lbs';
-                  }
-                  return label;
-                })
-              }
-            }
-          },
-          responsive: true,
-          maintainAspectRatio: false,
-          scales: {
-            x: {
-              type: 'time',
-            },
-            y: {
-              beginAtZero: false,
-              ticks: {
-                padding: 25,
-                callback: ((value) => {
-                  return value + ' lbs';
-                })
-              }
-            },
-          },
-        }
-      });
-    },
-    updateChart() {
-      this.colorIndex = 0;
-      if (this.chart) {
-        this.chart.destroy();
-        this.chart = null; // Set chart to null after destruction
+    updateData(interval) {
+      const intervalDate = new Date();
+      this.colorIndex = 0;  // reset colorIndex of the dataset colors array.
+      
+      if (interval === 'day') {
+        intervalDate.setDate(intervalDate.getDate() - 1);
+      } else if (interval === 'week') {
+        intervalDate.setDate(intervalDate.getDate() - 7);
+      } else if (interval === 'month') {
+        intervalDate.setMonth(intervalDate.getMonth() - 1);
+      } else if (interval === 'year') {
+        intervalDate.setFullYear(intervalDate.getFullYear() - 1);
       }
-      this.$nextTick(() => {
-        this.renderChart();
-      });
+
+      const formattedDate = intervalDate.toISOString();	  
+     
+      this.errorMessage = '';
+      this.loading = true;
+      this.fetchDataByDate(formattedDate)
+        .finally(() => {
+          this.loading = false;
+          this.chartKey = interval; // Update the chart key
+        });
     },
     setDaily() {
-      const oneDayAgo = new Date();
-      oneDayAgo.setDate(oneDayAgo.getDate() - 1);
-      const formattedDate = oneDayAgo.toISOString();
-
-      this.errorMessage = '';
-      this.fetchDataByDate(formattedDate);
+      this.chartOptions.scales.x.time.unit = 'hour';
+      this.updateData('day');
     },
     setWeekly() {
-      const oneWeekAgo = new Date();
-      oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-      const formattedDate = oneWeekAgo.toISOString();
-
-      this.errorMessage = '';
-      this.fetchDataByDate(formattedDate);
+      this.chartOptions.scales.x.time.unit = 'day';
+      this.updateData('week');
     },
     setMonthly() {
-      const oneMonthAgo = new Date();
-      oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
-      const formattedDate = oneMonthAgo.toISOString();
-
-      this.errorMessage = '';
-      this.fetchDataByDate(formattedDate);
+      this.chartOptions.scales.x.time.unit = 'day';
+      this.updateData('month');
     },
     setYearly() {
-      const oneYearAgo = new Date();
-      oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
-      const formattedDate = oneYearAgo.toISOString();
-
-      this.errorMessage = '';
-      this.fetchDataByDate(formattedDate);
+      this.chartOptions.scales.x.time.unit = 'month';
+      this.updateData('year');
     },
   },
 };
 </script>
 
 <style>
-.error-message {
-  color: red;
-}
+  h1 {
+    font-size: 1.5em;
+    font-weight: bold;
+    margin: 20px 0;
+  }
+  .alert {
+    padding: 1em;
+    width: 100%;
+    background-color: #F9F4E3;
+    font-size: 0.7rem;
+    font-family: sans-serif;
+    padding: 0.5rem;
+    text-align: center;
+    margin: 0rem 1rem;
+  }
+  .button-container {
+    display: flex;
+    background-color: #FAD33D;
+    margin: 0 1rem;
+  }
+  .button-container > button {
+    flex: 1;
+    padding: 0.7rem;
+    background-color: #BB3D04;
+    color: white;
+    border: none;
+    font-size: 0.8rem;
+    font-family: sans-serif;
+    text-transform: uppercase;
+    margin-right: 0.2rem;
+    margin-left: 0.2rem;
+  }
+  .chart-container {
+    background-color: #F9F4E3;
+    padding: 0.5rem;
+    margin: 1rem;
+  }
+  .error-message {
+    color: #F9F4E3;
+    background-color: #DC893B;
+    font-size: 0.7rem;
+    font-family: sans-serif;
+    padding: 0.5rem;
+    text-align: center;
+    margin: 0rem 1rem;
+  }
 </style>
