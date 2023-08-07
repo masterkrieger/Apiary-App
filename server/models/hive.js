be70f06a7c2e48678862861b91fd9a5fe943686b
Jeremy Barr <jeremybarr23@gmail.com>
2023-08-07 15:13:22 -0400

    Update hiveSchema
    -Update HiveSchema with firmware_version and Battery voltage

--- server/models/hive.js
+++ server/models/hive.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const hiveSchema = new mongoose.Schema({
   name: String,
   weight: Number,
-  date: Date
+  date: Date,
+  firmware_version: String,
+  battery: Number
 });
 
 module.exports = mongoose.model('Hive', hiveSchema);
